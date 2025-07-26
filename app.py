58730b7bf5a85ae7133f262f6886d354e32ada90
nageena12 <nageenashaik251@gmail.com>
2025-07-26 22:38:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 def check_even_odd(numbers):
     for num in numbers:
-        if num % 2 == 0:
+        if num % 4 == 0:
             print(f"{num} is Even")
         else:
             print(f"{num} is Odd")
